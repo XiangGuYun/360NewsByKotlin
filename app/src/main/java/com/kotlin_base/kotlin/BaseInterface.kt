49d652dd7de0c1aa4aaeaf171e265fa816462174
XiangGuYun <ye850570138@sina.com>
2018-07-24 00:15:40 +0800

    Initial commit

---
+++ app/src/main/java/com/kotlin_base/kotlin/BaseInterface.kt
@@ -0,0 +1,11 @@
+package com.zoga.yun.kotlin
+
+import android.support.v4.view.ViewPager
+import com.kotlin_base.kotlin.utils.OnPageChange
+import com.kotlin_base.kotlin.utils.OnTabSelected
+import com.kotlindemo.base.utils.*
+import com.zoga.yun.kotlin.utils.MessageUtils
+
+interface BaseInterface: SPUtils, TextViewUtils, StringUtils, BundleUtils,
+        DensityUtils, ViewUtils, CollectUtils, BmpUtils, RxPermissionUtils, NetUtils,
+        RVInterface, IOUtils, ContextUtils,MessageUtils,OnPageChange,OnTabSelected
